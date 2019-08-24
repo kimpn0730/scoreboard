788e62f3a55b08649bfda1088330b043cfad8446
kosmo_11 <rana3782@gmail.com>
2019-08-24 16:13:09 +0900

    페이징 방식 소스를 CRA으로 이전

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// 펑션 컴포넌트는 반드시 대문자로 시작
+// react elementt를 리턴해야 한다.
+
+const Header = ({title, totalPlayers}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="header">
+      <h1 className="h1">{title}</h1>
+      <span className="stats">Players : {totalPlayers}</span>
+    </header>
+  )
+};
+
+class Counter extends React.Component {
+  state = {
+    score: 0,
+    a: 10
+  };
+
+  constructor(){
+    super();
+    this.handleChangeScore = this.handleChangeScore.bind(this);
+  }
+
+  handleChangeScore(delta) {
+    console.log('incrementScore', this);
+    // state를 변경하는 방법은 setState() 밖에 없다.
+    // this.state.score += 1;
+    this.setState(prevState => ({score: this.state.score + delta}));
+  }
+
+  // 이벤트의 오른쪽은 함수 선언문이 들어가야 한다.
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={() => this.handleChangeScore(-1)}> -</button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={() => this.handleChangeScore(1)}> +</button>
+      </div>
+    )
+  }
 }
+
+const Player = (props) => (
+  <div className="player">
+		<span className="player-name">
+			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
+      {props.name}
+		</span>
+    <Counter />
+  </div>
+);
+
+//  function 컴포넌트에서 class 컴포넌트로 변경
+class App extends  React.Component {
+  state = {
+    players: [
+      {name: 'LDK', id: 1},
+      {name: 'HONG', id: 2},
+      {name: 'KIM', id: 3},
+      {name: 'PARK', id: 4},
+    ]
+  }
+  constructor(){
+    super();
+    this.handleRemovePlayer = this.handleRemovePlayer.bind(this);
+
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header title="My Scoreboard" totalPlayers={11}/>
+
+        {/*Player List*/}
+        {
+          this.state.players.map(player => {
+            return (
+              <Player name={player.name} key={player.id} id={player.id} removePlayer={this.handleRemovePlayer}/>
+            )
+          })
+        }
+      </div>
+    )
+  }
+  handleRemovePlayer(id) {
+    console.log(this);
+    console.log('handleRemovePlayer', id);
+    this.setState(prevState => {
+      const players = prevState.players.filter(player => player.id !== id)
+      return {players}
+    })
+  }
+}
+
+// 1. state를 가진 부모에서 삭제하는 콜백펑션을 만든다.
+// 2. 자식에게 콜백펑션을 props로 내려준다.
+// 3. 클릭시 자식이 콜백펑션을 호출
+// 4. prototype 완성
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -9,4 +9,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+// PWA 모드 활성화 원할 시 register();  후 meifest.json에서 아이콘 선택하면 스마트폰에서 아이콘 생성화면 생김
 serviceWorker.unregister();
