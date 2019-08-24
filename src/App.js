b24a9978d1039e9b3f912bbbb2a3df3dcab0f34e
kosmo_11 <rana3782@gmail.com>
2019-08-24 16:45:18 +0900

    리팩토링

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
 
-// 펑션 컴포넌트는 반드시 대문자로 시작
-// react elementt를 리턴해야 한다.
-
-const Header = ({title, totalPlayers}) => {
-  return (
-    <header className="header">
-      <h1 className="h1">{title}</h1>
-      <span className="stats">Players : {totalPlayers}</span>
-    </header>
-  )
-};
- 
 class Counter extends React.Component {
   state = {
     score: 0,
